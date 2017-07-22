eb51db030bfe723de0d45bd16d6a9fa955eb2c6f
Ahmed Elzoughby <gs.elzoughby@gmail.com>
2017-07-22 11:12:28 +0200

    code get optimized for space and time

--- App_base64.c
+++ App_base64.c
@@ -1,11 +1,42 @@
 #include <stdio.h>
 #include "base46.h"
 
-int main() {
+int main(int argc, char** argv) {
 
-    printf( base64_encode("Have to deal with Base64 format? Then this module is made for you. Welcome!") );
-    printf("\n");
-    printf( base64_decode("SGF2ZSB0byBkZWFsIHdpdGggQmFzZTY0IGZvcm1hdD8gVGhlbiB0aGlzIG1vZHVsZSBpcyBtYWRlIGZvciB5b3UuIFdlbGNvbWUh") );
+    if (argc == 1) {
 
-    return 0;
+        /* Have to deal with Base64 format? Welcome! */
+        printf(base64_decode("SGF2ZSB0byBkZWFsIHdpdGggQmFzZTY0IGZvcm1hdD8gV2VsY29tZSE="));
+        printf("\n=========================================");
+
+        printf("\n\n");
+        printf("Usage: base64 [options] [arguments] \n\n"
+                       "Options: \n\t"
+                       "encode \t Convert ASCII string into Base64 format string \n\t"
+                       "decode \t Convert Base64 format string into ASCII string \n"
+                       "Arguments: \n\t"
+                       "ASCII String \n\t"
+                       "Base64 String \n\n"
+        );
+
+        printf("\n");
+        printf(base64_decode(base64_encode("www.linkedin.com/in/elzoughby")));
+
+    } else if (argc == 3) {
+
+
+
+    } else {
+
+        printf("Wrong Parameters! \n\n"
+                       "Usage: base64 [options] [arguments] \n\n"
+                       "Options: \n\t"
+                       "encode \t Convert ASCII string into Base64 format string \n\t"
+                       "decode \t Convert Base64 format string into ASCII string \n"
+                       "Arguments: \n\t"
+                       "ASCII String \n\t"
+                       "Base64 String \n\n"
+        );
+
+    }
 }
--- base64.c
+++ base64.c
@@ -10,80 +10,59 @@ char base46_map[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
 char* base64_encode(char* plain) {
 
     char counts = 0;
-    char buffer[4];
+    char buffer[3];
     char* cipher = malloc(strlen(plain) * 4 / 3 + 4);
-    int i = 0;
     int c = 0;
 
-    while(plain[i] != '\0') {
-
-        if(counts == 0) {
-            buffer[0] = plain[i] >> 2;
-            counts++;
-        } else if(counts == 1) {
-            buffer[1] = ((plain[i - 1] & 0x03) << 4) + (plain[i] >> 4);
-            counts++;
-        } else if(counts == 2) {
-            buffer[2] = ((plain[i - 1] & 0x0f) << 2) + (plain[i] >> 6);
-            buffer[3] = plain[i] & 0x3f;
-            for(int j = 0; j < 4; j++) {
-                cipher[c] = base46_map[buffer[j]];
-                c++;
-            }
+    for(int i = 0; plain[i] != '\0'; i++) {
+        buffer[counts++] = plain[i];
+        if(counts == 3) {
+            cipher[c++] = base46_map[buffer[0] >> 2];
+            cipher[c++] = base46_map[((buffer[0] & 0x03) << 4) + (buffer[1] >> 4)];
+            cipher[c++] = base46_map[((buffer[1] & 0x0f) << 2) + (buffer[2] >> 6)];
+            cipher[c++] = base46_map[buffer[2] & 0x3f];
             counts = 0;
-        } else {
-            //do nothing
         }
-
-        i++;
     }
 
     if(counts > 0) {
-        if (counts == 1) {
-            buffer[1] = (plain[i - 1] & 0x03) << 4;
-            buffer[2] = '\0';
-            buffer[3] = '\0';
-        } else if (counts == 2) {
-            buffer[2] = ((plain[i - 1] & 0x0f) << 2);
-            buffer[3] = '\0';
+        cipher[c++] = base46_map[buffer[0] >> 2];
+        if(counts == 1) {
+            cipher[c++] = base46_map[(buffer[0] & 0x03) << 4];
+            cipher[c++] = '=';
+        } else {                      // if counts == 2
+            cipher[c++] = base46_map[((buffer[0] & 0x03) << 4) + (buffer[1] >> 4)];
+            cipher[c++] = base46_map[(buffer[1] & 0x0f) << 2];
         }
-
-        for (int j = 0; j < 4; j++) {
-            if (buffer[j] != '\0')
-                cipher[c] = base46_map[buffer[j]];
-            else
-                cipher[c] = '=';
-            c++;
-        }
+        cipher[c++] = '=';
     }
 
+    cipher[c] = '\0';   /* string padding character */
     return cipher;
 }
 
 
 char* base64_decode(char* cipher) {
 
-    char* plain = malloc(strlen(cipher) * 3 / 4);
-    char buffer[4];
     char counts = 0;
+    char buffer[4];
+    char* plain = malloc(strlen(cipher) * 3 / 4);
     int p = 0;
 
     for(int i = 0; cipher[i] != '\0'; i++) {
-
-        int k;
+        char k;
         for(k = 0 ; k < 64 && base46_map[k] != cipher[i]; k++);
         buffer[counts++] = k;
         if(counts == 4) {
-
             plain[p++] = (buffer[0] << 2) + (buffer[1] >> 4);
             if(buffer[2] != 64)
                 plain[p++] = (buffer[1] << 4) + (buffer[2] >> 2);
             if(buffer[3] != 64)
                 plain[p++] = (buffer[2] << 6) + buffer[3];
-
             counts = 0;
         }
     }
 
+    plain[p] = '\0';    /* string padding character */
     return plain;
 }
