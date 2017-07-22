78bd8253a7a0c8f836aa28d0c142e3b4fafaa52f
Ahmed Elzoughby <gs.elzoughby@gmail.com>
2017-07-22 15:31:57 +0200

    start supporting arguments

--- App_base64.c
+++ App_base64.c
@@ -1,42 +1,41 @@
 #include <stdio.h>
 #include "base46.h"
 
+
+void print_usage();
+
+
 int main(int argc, char** argv) {
 
     if (argc == 1) {
 
         /* Have to deal with Base64 format? Welcome! */
-        printf(base64_decode("SGF2ZSB0byBkZWFsIHdpdGggQmFzZTY0IGZvcm1hdD8gV2VsY29tZSE="));
-        printf("\n=========================================");
-
-        printf("\n\n");
-        printf("Usage: base64 [options] [arguments] \n\n"
-                       "Options: \n\t"
-                       "encode \t Convert ASCII string into Base64 format string \n\t"
-                       "decode \t Convert Base64 format string into ASCII string \n"
-                       "Arguments: \n\t"
-                       "ASCII String \n\t"
-                       "Base64 String \n\n"
-        );
-
+        printf( base64_decode("SGF2ZSB0byBkZWFsIHdpdGggQmFzZTY0IGZvcm1hdD8gV2VsY29tZSE="));
         printf("\n");
-        printf(base64_decode(base64_encode("www.linkedin.com/in/elzoughby")));
+        printf( base64_decode( base64_encode("www.linkedin.com/in/elzoughby")));
+        printf("\n=========================================\n\n");
+        print_usage();
 
     } else if (argc == 3) {
 
-
+        
 
     } else {
 
-        printf("Wrong Parameters! \n\n"
-                       "Usage: base64 [options] [arguments] \n\n"
-                       "Options: \n\t"
-                       "encode \t Convert ASCII string into Base64 format string \n\t"
-                       "decode \t Convert Base64 format string into ASCII string \n"
-                       "Arguments: \n\t"
-                       "ASCII String \n\t"
-                       "Base64 String \n\n"
-        );
+        printf("Wrong Parameters! \n\n");
+        print_usage();
 
     }
+}
+
+
+void print_usage() {
+    printf("Usage: base64 [options] [arguments] \n\n"
+                   "Options: \n\t"
+                   "encode \t Convert ASCII string into Base64 format string \n\t"
+                   "decode \t Convert Base64 format string into ASCII string \n"
+                   "Arguments: \n\t"
+                   "ASCII String \n\t"
+                   "Base64 String \n\n"
+    );
 }
