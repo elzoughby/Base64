3049b2a507cf0ad61f8106f4e3b9151e6e546f56
Ahmed Elzoughby <gs.elzoughby@gmail.com>
2017-07-23 11:21:04 +0200

    added some comments

--- base64.h
+++ base64.h
@@ -1,3 +1,10 @@
+    /***********************************************************
+    * Base64 library                                           *
+    * @author Ahmed Elzoughby                                  *
+    * @date July 23, 2017                                      *
+    * Purpose: encode and decode base64 format                 *
+    ***********************************************************/
+
 #ifndef BASE46_H
 #define BASE46_H
 
@@ -5,8 +12,19 @@
 #include <memory.h>
 
 
+/***********************************************
+Encodes ASCCI string into base64 format string
+@param plain ASCII string to be encoded
+@return encoded base64 format string
+***********************************************/
 char* base64_encode(char* plain);
 
+
+/***********************************************
+decodes base64 format string into ASCCI string
+@param plain encoded base64 format string
+@return ASCII string to be encoded
+***********************************************/
 char* base64_decode(char* cipher);
 
 
--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+    /***********************************************************
+    * Base64 example app                                       *
+    * @author Ahmed Elzoughby                                  *
+    * @date July 23, 2017                                      *
+    * Purpose: Demonstration of Base64 library                 *
+    * Usage: base64 (encode | decode) <source> [<destination>] *
+    ***********************************************************/
+
 #include <stdio.h>
 #include <stdbool.h>
 #include "base64.h"
@@ -5,10 +13,12 @@
 
 void usage();
 void welcome();
-void three_args(char* opt, char* src);
-void four_args(char* opt, char* src, char* dest);
+void do_and_print(char *opt, char *src);
+void do_and_save(char *opt, char *src, char *dest);
 bool file_exists(char* path);
 void args_error();
+
+
 
 
 int main(int argc, char* argv[]) {
@@ -16,23 +26,24 @@ int main(int argc, char* argv[]) {
     if (argc == 1)
         welcome();
     else if (argc == 3)
-        three_args(argv[1], argv[2]);
+        do_and_print(argv[1], argv[2]);
     else if (argc == 4)
-        four_args(argv[1], argv[2], argv[3]);
+        do_and_save(argv[1], argv[2], argv[3]);
     else
         args_error();
 
-    return 0;
 }
 
 
 void welcome() {
     printf("===============================================================\n");
-    printf( base64_decode("SGF2ZSB0byBkZWFsIHdpdGggQmFzZTY0IGZvcm1hdD8gV2VsY29tZSE="));
+    printf("%s", base64_decode("SGF2ZSB0byBkZWFsIHdpdGggQmFzZTY0IGZvcm1hdD8gV2VsY29tZSE="));
     printf("\n===============================================================\n\n");
+
     usage();
+
     printf("\n===============================================================\n");
-    printf( base64_decode( base64_encode("www.linkedin.com/in/elzoughby")));
+    printf("%s", base64_decode( base64_encode("www.linkedin.com/in/elzoughby")));
     printf("\n===============================================================\n");
 }
 
@@ -43,13 +54,13 @@ void usage() {
                    "encode \t\t\t Convert ASCII string into Base64 format \n\t"
                    "decode \t\t\t Convert Base64 format into ASCII string \n"
                    "Arguments: \n\t"
-                   "source \t\t\t String or the path/to/file to be converted  \n\t"
+                   "source \t\t\t String or path/to/file to be converted  \n\t"
                    "destination \t path/to/converted/file \n"
     );
 }
 
 
-void three_args(char* opt, char* src) {
+void do_and_print(char *opt, char *src) {
 
     if(file_exists(src)) {
         FILE* file = fopen(src, "r");
@@ -68,16 +79,16 @@ void three_args(char* opt, char* src) {
         if(line) free(line);
     } else {
         if(strcmp("encode", opt) == 0)
-            printf(base64_encode(src));
+            printf("%s", base64_encode(src));
         else if(strcmp("decode", opt) == 0)
-            printf(base64_decode(src));
+            printf("%s", base64_decode(src));
         else
             args_error();
     }
 }
 
 
-void four_args(char *opt, char *src, char *dest) {
+void do_and_save(char *opt, char *src, char *dest) {
     FILE* dest_file = fopen(dest, "w");
     if(dest_file == NULL) exit(1);
     if(file_exists(src)) {
@@ -90,16 +101,16 @@ void four_args(char *opt, char *src, char *dest) {
                 fprintf(dest_file, "%s\n" ,base64_encode(line));
         else if(strcmp("decode", opt) == 0)
             while (getline(&line, &len, src_file) != -1)
-                fprintf(dest_file, "%s\n" ,base64_decode(line));
+                fprintf(dest_file, "%s\n", base64_decode(line));
         else
             args_error();
         fclose(src_file);
         if(line) free(line);
     } else {
         if(strcmp("encode", opt) == 0)
-            fprintf(dest_file, base64_encode(src));
+            fprintf(dest_file, "%s", base64_encode(src));
         else if(strcmp("decode", opt) == 0)
-            fprintf(dest_file ,base64_decode(src));
+            fprintf(dest_file , "%s", base64_decode(src));
         else
             args_error();
     }
@@ -119,6 +130,6 @@ bool file_exists(char* path) {
 
 
 void args_error() {
-    printf("Wrong Parameters! \n");
+    printf("Wrong Parameters! \n\n");
     usage();
 }
